0871a93b616e6ea731a09407c6496c0c83bd439f
Marimg0528 <160878613+marimg0528@users.noreply.github.com>
2024-02-22 10:20:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,16 @@ from PIL import Image
 
 st.title("Página Web UwU")
 
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Mult2.png)
 
+st.image(image, caption = 'Interfaces multimodales')
+
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
